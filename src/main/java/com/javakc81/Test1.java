dae01fa46ff7bd01274f72c7bbed6d07e2eeb6a4
wangpeng <wangpeng@wangpeng.com>
2020-09-25 14:58:04 +0800

    冲突1

--- src/main/java/com/javakc81/Test1.java
+++ src/main/java/com/javakc81/Test1.java
@@ -5,6 +5,7 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("javakc81 hello1");
+        System.out.println("javakc81 hello2");
     }
 
 }
