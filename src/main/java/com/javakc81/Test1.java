ed47ef28f633bc7ddd4b2e1b786fe39009f0a409
wangpeng <wangpeng@wangpeng.com>
2020-09-25 14:53:30 +0800

    IDEA中的冲突制造

--- src/main/java/com/javakc81/Test1.java
+++ src/main/java/com/javakc81/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 
     public static void main(String[] args) {
         System.out.println("123");
-        System.out.println("123");
+        System.out.println("javakc81 hello");
     }
 
 }
