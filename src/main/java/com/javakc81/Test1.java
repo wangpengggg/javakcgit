a48cb558195bd71c3cc53716933c092259946993
wangpeng <wangpeng@wangpeng.com>
2020-09-25 15:02:44 +0800

    IDEA中的冲突制造2

--- src/main/java/com/javakc81/Test1.java
+++ src/main/java/com/javakc81/Test1.java
@@ -3,7 +3,7 @@ package com.javakc81;
 public class Test1 {
 
     public static void main(String[] args) {
-        System.out.println("123");
+        System.out.println("666");
         System.out.println("javakc81 hello1");
         System.out.println("javakc81 hello2");
     }
