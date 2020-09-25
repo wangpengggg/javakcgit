54bbfd32854724b8d7fd90c4f84314f8be6a9803
wangpeng <wangpeng@wangpeng.com>
2020-09-25 15:16:06 +0800

    解决冲突 暂存

--- src/main/java/com/javakc81/Test1.java
+++ src/main/java/com/javakc81/Test1.java
@@ -4,6 +4,7 @@ public class Test1 {
 
     public static void main(String[] args) {
         System.out.println("666");
+        System.out.println("777");
         System.out.println("javakc81 hello1");
         System.out.println("javakc81 hello2");
     }
